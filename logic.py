dc7407c4709d1cb91ffbaca3bc88a6d3a4cd59f0
m-uvex <musamurad@proton.me>
2026-01-19 02:52:37 +0300

    Added files listing, sorting and selecting

--- logic.py
+++ logic.py
@@ -5,18 +5,26 @@ scanning = True
 watched_dir = os.path.expanduser('~/Downloads')
 scan_interval = 5
 
+print('Scanning...')
+print()
+
 while scanning:
-    count = 0
-    print('scanning')
+    files = []
     for filename in os.listdir(watched_dir):
         if filename.endswith('.AppImage'):
             filepath = os.path.join(watched_dir, filename)
-            # LOGIC HERE
-            print('File found: ', filename)
-            count += 1
+            found_file = filename
+            files.append(filename)
+            files.sort()
 
-    if count > 0:
-        print('> Found ', count, " files.")
+    if files:
+        print('> Found', len(files), 'files.')
+        for i, filename in enumerate(files, 1):
+            print(f'{i}  -  {filename}')
+
+        print()
+        print('Files to setup (eg: 1 2 3, 1-3 or all)')
+        target = input()
         scanning = False
     else:
         time.sleep(scan_interval)
