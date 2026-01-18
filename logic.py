b407b567036d636af5ffb34cee8dab931d0e5232
m-uvex <musamurad@proton.me>
2026-01-19 01:52:04 +0300

    Reworked file scanning, made it stop once it finds AppImages

--- logic.py
+++ logic.py
@@ -1,15 +1,24 @@
 import time
 import os
 
+scanning = True
 watched_dir = os.path.expanduser('~/Downloads')
-scan_interval = 1
+scan_interval = 5
 
-while True:
+while scanning:
+    count = 0
     print('scanning')
     for filename in os.listdir(watched_dir):
         if filename.endswith('.AppImage'):
             filepath = os.path.join(watched_dir, filename)
             # LOGIC HERE
             print('File found: ', filename)
+            count += 1
 
-    time.sleep(scan_interval)
+    if count > 0:
+        print('> Found ', count, " files.")
+        scanning = False
+    else:
+        time.sleep(scan_interval)
+        print('No files found')
+        print()
