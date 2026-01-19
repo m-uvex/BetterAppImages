ced3a0695882be7b83214d09977500a0a51d1a7e
m-uvex <musamurad@proton.me>
2026-01-19 04:16:43 +0300

    made scanning not last forever, added a rescan option and max tries per scan variable

--- logic.py
+++ logic.py
@@ -3,13 +3,16 @@ import os
 
 scanning = True
 watched_dir = os.path.expanduser('~/Downloads')
-scan_interval = 5
+scan_interval = 3
+max_tries = 3
 
-print('Scanning...')
-print()
+files = []
+tries = 0
+
+if scanning:
+    print('Scanning...\n')
 
 while scanning:
-    files = []
     for filename in os.listdir(watched_dir):
         if filename.endswith('.AppImage'):
             filepath = os.path.join(watched_dir, filename)
@@ -18,15 +21,24 @@ while scanning:
             files.sort()
 
     if files:
+        scanning = False
         print('> Found', len(files), 'files.')
         for i, filename in enumerate(files, 1):
             print(f'{i}  -  {filename}')
 
-        print()
-        print('Files to setup (eg: 1 2 3, 1-3 or all)')
+        print('\nFiles to setup (eg: 1 2 3, 1-3 or all)')
         target = input()
-        scanning = False
-    else:
+        
+    elif tries >= max_tries:
+        if len(files) < 1:
+            scanning = False
+            tries = 0
+            confirmation = input('No files found, rescan? [y/n] ')
+            if confirmation.lower() in ["y", "yes", ""]:
+                scanning = True
+                print('\nScanning...\n')
+            else: exit()
+
+    elif tries < max_tries:
+        tries += 1
         time.sleep(scan_interval)
-        print('No files found')
-        print()
